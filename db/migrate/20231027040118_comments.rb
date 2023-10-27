cd76112d39703248f0a4f9c9b15304066916f94e
Newton Ombese <newtonombese1@gmail.com>
2023-10-27 07:30:43 +0300

    Add migration files

---
+++ db/migrate/20231027040118_comments.rb
@@ -0,0 +1,13 @@
+class Comments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+      t.string :text, null: false
+    
+      t.timestamps
+    end
+    add_index :comments, :user_id
+    add_index :comments, :post_id    
+  end
+end
