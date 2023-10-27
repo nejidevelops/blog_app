758e9209ebe539891614a7308929af60e64f6cc0
Newton Ombese <newtonombese1@gmail.com>
2023-10-27 14:44:31 +0300

    Update like model file as suggested

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,12 @@ class Like < ApplicationRecord
 
   validates :user_id, presence: true
   validates :post_id, presence: true
+
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
