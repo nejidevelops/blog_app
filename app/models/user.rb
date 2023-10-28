03231a60d42e1bdec054cf7acad26649b706c6b3
Newton Ombese <newtonombese1@gmail.com>
2023-10-28 18:18:42 +0300

    Successful working on the user test file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def three_most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+  def recent_posts
+    posts.limit(3).order(created_at: :desc)
   end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is valid with a name' do
+    user = User.new(name: 'John', posts_counter: 0)
+    expect(user).to be_valid
+  end  
+
+  it 'is not valid without a name' do
+    user = User.new
+    expect(user).to be_invalid
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is valid with a non-negative posts_counter' do
+    user = User.new(name: 'Alice', posts_counter: 0)
+    expect(user).to be_valid
+  end
+
+  describe '#recent_posts' do
+    it 'returns the most recent 3 posts' do
+
+      your_class = User.new
+      posts = double('posts')
+      allow(your_class).to receive(:posts).and_return(posts)
+      allow(posts).to receive(:limit).with(3).and_return(posts)
+      allow(posts).to receive(:order).with(created_at: :desc).and_return(posts)
+
+      expect(your_class.recent_posts).to eq(posts)
+    end
+  end
+end
