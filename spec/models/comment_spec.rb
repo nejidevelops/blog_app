38881d5521304bb5b9ab17725314b721776ac5a5
Newton Ombese <newtonombese1@gmail.com>
2023-11-02 11:57:14 +0300

    [FIX] Work on rubocop errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,41 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:user) { User.create(name: "Test User") }
-  let(:post) { Post.create(title: "Sample Post") }
+  let(:user) { User.create(name: 'Test User') }
+  let(:post) { Post.create(title: 'Sample Post') }
 
-  describe "validations" do
-    it "is valid with valid attributes" do
+  describe 'validations' do
+    it 'is valid with valid attributes' do
       comment = Comment.new(
         author: user,
         post: post,
-        text: "A valid comment",
+        text: 'A valid comment',
         created_at: Time.now,
         updated_at: Time.now
       )
       expect(comment).to be_valid
     end
 
-    it "is not valid without an author" do
-      comment = Comment.new(author: nil, post: post, text: "A comment")
+    it 'is not valid without an author' do
+      comment = Comment.new(author: nil, post: post, text: 'A comment')
       expect(comment).not_to be_valid
     end
 
-    it "is not valid without a post" do
-      comment = Comment.new(author: user, post: nil, text: "A comment")
+    it 'is not valid without a post' do
+      comment = Comment.new(author: user, post: nil, text: 'A comment')
       expect(comment).not_to be_valid
     end
   end
 
-  describe "associations" do
-    it "belongs to an author (User)" do
+  describe 'associations' do
+    it 'belongs to an author (User)' do
       association = Comment.reflect_on_association(:author)
       expect(association.macro).to eq :belongs_to
       expect(association.options[:class_name]).to eq 'User'
       expect(association.options[:foreign_key]).to eq 'author_id'
     end
 
-    it "belongs to a post" do
+    it 'belongs to a post' do
       association = Comment.reflect_on_association(:post)
       expect(association.macro).to eq :belongs_to
     end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:user) { User.create(name: "Test User") }
-  let(:post) { Post.create(title: "Sample Post") }
+  let(:user) { User.create(name: 'Test User') }
+  let(:post) { Post.create(title: 'Sample Post') }
 
-  describe "validations" do
-    it "is not valid without a user" do
+  describe 'validations' do
+    it 'is not valid without a user' do
       like = Like.new(user: nil, post: post)
       expect(like).not_to be_valid
     end
 
-    it "is not valid without a post" do
+    it 'is not valid without a post' do
       like = Like.new(user: user, post: nil)
       expect(like).not_to be_valid
     end
   end
 
-  describe "associations" do
-    it "belongs to a user" do
+  describe 'associations' do
+    it 'belongs to a user' do
       association = Like.reflect_on_association(:user)
       expect(association.macro).to eq :belongs_to
     end
 
-    it "belongs to a post" do
+    it 'belongs to a post' do
       association = Like.reflect_on_association(:post)
       expect(association.macro).to eq :belongs_to
     end
