b6c8f1e5ddf59bc1a720115810a48be2254c9b1b
Newton Ombese <newtonombese1@gmail.com>
2023-11-08 10:17:55 +0300

    Add a default value for the user's post counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_223837) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_071413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_223837) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
