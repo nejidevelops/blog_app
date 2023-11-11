04c128518969af45a30dfdd66689bab7bec5a898
Newton Ombese <newtonombese1@gmail.com>
2023-11-11 09:23:02 +0300

    Update my model association to user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post, required: true
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id', required: true
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id', required: true
 
   after_create :update_comment_counter
 
