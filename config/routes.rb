2065ea054a29124d87f41c87dfa13cec99b24841
Newton Ombese <newtonombese1@gmail.com>
2023-11-11 19:22:35 +0300

    Add route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   resources :users do
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
   end
 end
